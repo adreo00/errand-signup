fb7e37ae7958968001da1801d27711cc1bca6917
Alex <22608380+meagerYak@users.noreply.github.com>
2020-03-29 01:36:18 -0400

    init dynamoDB

--- src/actions/submit.js
+++ src/actions/submit.js
@@ -1,4 +1,17 @@
-import { axios } from "axios"
+// import { axios } from "axios"
 
-axios.request
+// axios.request
+
+import awsconfig from "./aws-exports"
+
+Amplify.configure(awsconfig)
+import Amplify, { Analytics, Storage, API } from "aws-amplify"
+
+// Configure Amplify
+API.configure(awsconfig)
+Storage.configure(awsconfig)
+
+var dynamodb = new AWS.DynamoDB()
+Storage.put()
+
 export default submit
